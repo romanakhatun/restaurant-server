64840a884d2518e316163423aab18e29de1980eb
romanakhatun <romanakhatun2004@gmail.com>
2021-12-20 10:40:23 +0600

    data add

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
 
+const { MongoClient } = mongodb;
 const app = express();
+dotenv.config();
 
-app.get("/", (request, response) => {
-  response.send("thank you very much");
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-app.get("/foods", (request, response) => {
-  response.send("this is food page");
+const uri = `mongodb+srv://restaurant:${process.env.DB_PASS}@cluster0.tfkl8.mongodb.net/foodsStore?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const foodsCollection = client.db("foodsStore").collection("foods");
+  console.log("database connected");
+
+  // const food = { name: "kanana", price: 25 };
+  // foodsCollection.insertOne(food, (err, res) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log(res);
+  //   }
+  // });
+
+  app.post("/addFood", (req, res) => {
+    const foods = req.body;
+    foodsCollection.insertMany(foods).then((result) => {
+      console.log(result.insertedCount);
+      res.send(result.insertedCount);
+    });
+  });
+
+  app.get("/foods", (req, res) => {
+    foodsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  console.log(err);
 });
 
 const PORT = 5000;
